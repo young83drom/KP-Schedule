9f307e9c7329ed44e0975be33f35447920190554
young83drom <young83drom@outlook.com>
2026-07-01 13:58:50 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>KP Schedule</h1>
+      <span className="system-status">System Operational</span>
+    </header>
+  );
+}
+
+export default Header;
